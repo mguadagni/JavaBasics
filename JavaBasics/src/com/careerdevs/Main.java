46b1f9152c24d17fedf659149804f2beb4cc09e0
Michael Guadagni <guadagnimichael@gmail.com>
2022-01-03 21:02:08 -0500

    Challenge 2/3

--- JavaBasics/src/com/careerdevs/Main.java
+++ JavaBasics/src/com/careerdevs/Main.java
@@ -3,10 +3,17 @@ package com.careerdevs;
 public class Main {
 
     public static void main(String[] args) {
-	String firstName = "Michael ";
-    String lastName = "Guadagni ";
-    String fullName = firstName + lastName;
-    System.out.println(fullName);
+        //Challenge 1
+	    /*String firstName = "Michael ";
+        String lastName = "Guadagni ";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);*/
+        System.out.println(createFullName("Michael","Guadagni"));
+
+    }
+    //Challenge 2
+    public static String createFullName(String firstName, String lastName) {
+        return firstName + " " + lastName;
     }
 
 }
