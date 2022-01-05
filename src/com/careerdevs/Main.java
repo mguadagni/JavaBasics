e2d2341d9ace4ebcc34274cdb5118a0023f0750d
Michael Guadagni <guadagnimichael@gmail.com>
2022-01-05 15:39:51 -0500

    Completed challenge 3/3 of JavaBasics

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -4,16 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
         //Challenge 1
-	    /*String firstName = "Michael ";
+        String firstName = "Michael ";
         String lastName = "Guadagni ";
         String fullName = firstName + lastName;
-        System.out.println(fullName);*/
-        System.out.println(createFullName("Michael","Guadagni"));
+        System.out.println(fullName);
 
+        //Challenge 2
+        System.out.println(createFullName("Michael", "Guadagni"));
+
+        //Challenge 3
+        String generatedName = NameGenerator.generateFullName("first", "last");
+        System.out.println(generatedName);
     }
+
     //Challenge 2
-    public static String createFullName(String firstName, String lastName) {
-        return firstName + " " + lastName;
+    public static String createFullName(String fName, String lName) {
+        return fName + " " + lName;
     }
-
 }
+
+
---
+++ src/com/careerdevs/NameGenerator.java
@@ -0,0 +1,9 @@
+package com.careerdevs;
+
+//Challenge 3
+public class NameGenerator {
+
+    public static String generateFullName (String firstName, String lastName) {
+        return firstName + " " + lastName;
+    }
+}
