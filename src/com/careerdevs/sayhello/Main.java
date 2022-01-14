b8810ab47561b6a00446c73e23ce5e4baeebfdfd
Michael Guadagni <guadagnimichael@gmail.com>
2022-01-13 20:24:03 -0500

    Completed say hello to the user challenge

---
+++ src/com/careerdevs/sayhello/Main.java
@@ -0,0 +1,30 @@
+package com.careerdevs.sayhello;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        whatIsYourFullName();
+    }
+
+    public static void whatIsYourName() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String userInput = scanner.nextLine();
+
+        System.out.println("Hello, nice to meet you " + userInput);
+    }
+    public static void whatIsYourFullName() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your first name? ");
+        String firstName = scanner.nextLine();
+
+        System.out.println("What is your last name? ");
+        String lastName = scanner.nextLine();
+
+        System.out.println("It is nice to formally meet you, " + firstName + " " + lastName);
+    }
+
+}
